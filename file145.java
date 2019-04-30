ff2673244e0cd970a19b54f3e4ebe79fb6b11668
dimwael <32783348+dimwael@users.noreply.github.com>
2019-04-30 13:28:28 -0400

    Update file145.java

--- file145.java
+++ file145.java
@@ -3,8 +3,14 @@ package com.ml;
 public class Dog {
    String breed;
    int age;
+   int id ;
    String color;
-
+   
+   public void getAge()
+   {return age;}
+   
+   public void getColor()
+   {return color;}
    void barking() {
    }
 
