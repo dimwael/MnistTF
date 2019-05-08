1c2bd98c925f2c7573ee5f7373f0c71e1c3a3a7a
wael <dim.wael@gmail.com>
2019-05-08 17:29:18 -0400

    bot_commit: 080519-052918.696

--- file134.java
+++ file134.java
@@ -1,3 +1,5 @@
+->RefactoringNumber->53837<-MoveField(Class_3;com.ml.Cat;[age];[])
+<-endRefactoring marker->
 package com.ml;
 
 public class Cat {
--- file145.java
+++ file145.java
@@ -1,3 +1,12 @@
+->RefactoringNumber->53825<-EncapsulateField(com.ml.Dog;;[age];[])
+->RefactoringNumber->53826<-EncapsulateField(com.ml.Dog;;[id];[])
+->RefactoringNumber->53828<-ExtractSubClass(com.ml.Dog;Class_1;[age|breed];[barking])
+->RefactoringNumber->53829<-PullUpMethod(Class_1;com.ml.Dog;[];[getAge])
+->RefactoringNumber->53830<-ExtractSubClass(com.ml.Dog;Class_2;[age];[hungry])
+->RefactoringNumber->53832<-IncreaseMethodSecurity(com.ml.Dog;;[];[getColor])
+->RefactoringNumber->53834<-PushDownField(com.ml.Dog;Class_2;[color];[])
+->RefactoringNumber->53836<-ExtractSubClass(com.ml.Dog;Class_4;[id];[])
+<-endRefactoring marker->
 package com.ml;
 
 public class Dog {
