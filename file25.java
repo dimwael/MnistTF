dcc2052d0f41ffcb015b8d8697d8f424efb15847
wael <dim.wael@gmail.com>
2019-05-09 15:41:45 -0400

    bot_commit: 090519-034145.618

--- file25.java
+++ file25.java
@@ -1,3 +1,14 @@
+->RefactoringNumber->60264<-ExtractSubClass(com.ml.bird;Class_1;[];[barkingPerhaps])
+->RefactoringNumber->60266<-ExtractClass(com.ml.bird;Class_2;[];[])
+->RefactoringNumber->60267<-PullUpMethod(Class_1;com.ml.bird;[];[barkingPerhaps])
+->RefactoringNumber->60268<-PushDownField(com.ml.bird;Class_1;[class_2];[])
+->RefactoringNumber->60269<-PushDownMethod(com.ml.bird;Class_1;[];[hungry])
+->RefactoringNumber->60271<-ExtractSubClass(com.ml.bird;Class_3;[];[sleeping])
+->RefactoringNumber->60273<-PushDownMethod(com.ml.bird;Class_3;[];[barkingPerhaps])
+->RefactoringNumber->60275<-PullUpMethod(Class_3;com.ml.bird;[];[barkingPerhaps])
+->RefactoringNumber->60276<-PullUpField(Class_1;com.ml.bird;[class_2];[])
+->RefactoringNumber->60277<-MoveField(default.Dog;com.ml.bird;[age];[])
+<-endRefactoring marker->
 package com.ml;
 
 public class bird {
